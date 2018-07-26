347b8c5c2b6be2e7cdfb35a8bf57ac03421f5270
Edgar Pino <edgarbeltran@me.com>
2018-07-26 15:47:06 -0600

    add first chart for mpg by model

---
+++ src/main.js
@@ -0,0 +1,56 @@
+import * as d3 from 'd3'
+import { fuelTypeColors } from './colors'
+
+const mpgByModelDataUrl = require('./data/mpg-by-model.csv')
+
+document.addEventListener('DOMContentLoaded', () => {
+  d3.csv(mpgByModelDataUrl, (data) => {
+    createMPGbyModelChart(data)
+  })
+})
+
+function createMPGbyModelChart(data) {
+  const barHeight = 23
+  const chartWidth = 950
+  const chartXOffset = 300
+
+  const dataValues = data.map((d) => d['Avg. City08'])
+
+  const chartX = d3.scaleLinear()
+    .domain([0, d3.max(dataValues)])
+    .range([0, chartWidth]);
+
+  const chart = d3.select('.mpgByModel')
+    .attr('width', chartWidth)
+    .attr('height', barHeight * data.length)
+
+  const bar = chart.selectAll('g')
+    .data(data)
+    .enter().append('g')
+    .attr('transform', (d, i) => `translate(0,${i * barHeight})`)
+
+  bar.append('line')
+    .attr('x1', '0')
+    .attr('x2', chartXOffset)
+    .attr('y1', '0')
+    .attr('y2', '0')
+    .attr('stroke', 'black')
+
+  bar.append('rect')
+    .attr('width', (item) => item['Avg. City08'] * 10)
+    .attr('height', barHeight - 4)
+    .attr('x', (d) => chartXOffset)
+    .attr('fill', (d) => fuelTypeColors[d['Fuel Type']])
+
+  bar.append('text')
+    .attr('x', (d) => d['Avg. City08'] * 10 + 5 + chartXOffset)
+    .attr('y', barHeight / 2)
+    .attr('dy', '.3em')
+    .text((d) => Math.round(d['Avg. City08'] * 100) / 100)
+
+  bar.append('text')
+    .attr('x', (d) => 5)
+    .attr('y', barHeight / 2)
+    .attr('dy', '.3em')
+    .text((d) => `${d.Make} ${d.Model}`)
+}
