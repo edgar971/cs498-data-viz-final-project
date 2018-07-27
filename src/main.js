55f1b6ca630953ab71bee7b57a1765f3c1590b63
Edgar Pino <edgarbeltran@me.com>
2018-07-26 23:52:45 -0600

    worked on layout and overview table

--- src/main.js
+++ src/main.js
@@ -1,13 +1,80 @@
 import * as d3 from 'd3'
 import { fuelTypeColors } from './colors'
 
-const mpgByModelDataUrl = require('./data/mpg-by-model.csv')
+const appStoreDatasetUrl = require('./data/appstore.csv')
+let appStoreData
 
 document.addEventListener('DOMContentLoaded', () => {
-  d3.csv(mpgByModelDataUrl, (data) => {
-    createMPGbyModelChart(data)
+  d3.csv(appStoreDatasetUrl, (data) => {
+    appStoreData = data
+    const overviewData = createOverviewData(data)
+    createOverviewTable(overviewData)
   })
 })
+
+
+function createOverviewTable(data) {
+  const headers = {
+    category: 'Category',
+    total: 'Number of Apps',
+    total_reviews: 'Number of Reviews',
+    user_rating: 'User Rating Average'
+  }
+  const table = d3.select('.overview')
+    .attr('width', '100%')
+
+  const thead = table.append('thead')
+  const tbody = table.append('tbody')
+
+  thead.append('tr')
+    .selectAll('th')
+    .data(Object.values(headers)).enter()
+    .append('th')
+    .text((header) => header)
+
+  const rows = tbody.selectAll('tr')
+    .data(data)
+    .enter()
+    .append('tr')
+
+  rows.selectAll('td')
+    .data((row) => {
+      return Object.keys(headers)
+        .map(header => ({ column: header, value: row[header] }))
+    })
+    .enter()
+    .append('td')
+    .text(d => d.column === 'total_reviews' ? d.value.toLocaleString() : d.value)
+}
+
+function createOverviewData(data) {
+  let overviewData = Object.values(data.reduce((overview, i) => {
+    if (!!overview[i.prime_genre]) {
+      const { total, total_reviews, user_rating } = overview[i.prime_genre]
+      overview[i.prime_genre] = {
+        ...overview[i.prime_genre],
+        total_reviews: total_reviews + parseInt(i.rating_count_tot),
+        user_rating: (user_rating + parseFloat(i.user_rating)),
+        total: total + 1,
+      }
+    } else {
+      overview[i.prime_genre] = {
+        category: i.prime_genre,
+        total_reviews: parseInt(i.rating_count_tot),
+        user_rating: parseFloat(i.user_rating),
+        total: 1
+      }
+    }
+    return overview
+  }, {}))
+
+  overviewData = overviewData.map(i => ({
+    ...i,
+    user_rating: Math.round((i.user_rating / i.total) * 100) / 100
+  }))
+
+  return overviewData
+}
 
 function createMPGbyModelChart(data) {
   const barHeight = 23
@@ -32,8 +99,8 @@ function createMPGbyModelChart(data) {
   bar.append('line')
     .attr('x1', '0')
     .attr('x2', chartXOffset)
-    .attr('y1', '0')
-    .attr('y2', '0')
+    .attr('y1', '1')
+    .attr('y2', '1')
     .attr('stroke', 'black')
 
   bar.append('rect')
